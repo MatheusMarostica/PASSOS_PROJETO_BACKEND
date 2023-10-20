4e54309972f3fed09f121d096458c8535c42e3ff
MatheusMarostica <matheus.marostica.lemos@gmail.com>
2023-10-20 14:07:52 -0300

    Rotas do Projeto

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
 const router = require('./routes/rotas');
-app.use('/api', router);
 
 app.set('port', process.env.PORT || 3333);
+app.use('/api', router);
 
 module.exports = app;
